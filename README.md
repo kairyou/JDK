1cc8de0160f628f88573e820c0fcf5b907864d22
Leon Xu <xhl@live.it>
2013-06-07 22:06:51 +0800

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+把JDK文件夹放到D盘符 (D:/JDK)  
+直接运行set-windows.cmd即可快速设置java(JDK6)到系统环境变量.
---
+++ set-Mac-VirtualMachine.cmd
@@ -0,0 +1,15 @@
+@echo off
+color b0
+set JDK_PATH=Z:\leon\D\WebDevelopment\JDK
+if "%JAVA_HOME%" == "" (
+    setx JAVA_HOME "%JDK_PATH%"
+    setx /M Path "%Path%;%JDK_PATH%\bin"
+    echo.
+    echo JDK 环境变量设置完毕..
+    echo.
+) else (
+    echo.
+    echo JDK 环境变量已设置过.
+    echo.
+)
+pause
